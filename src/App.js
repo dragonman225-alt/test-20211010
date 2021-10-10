45fef511a68492ccb5c58e55194a346dd9ec9121
Alexander Wang <zxcvb22217@gmail.com>
2021-10-10 18:34:49 +0800

    docs: Add comment for state management

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function App() {
   const [state, dispatch] = useReducer(appStateReducer, defaultAppState);
 
   return (
+    /**
+     * For simplicity, I inject `dispatch` to the context so the
+     * components can call it.
+     *
+     * An improvement would be wrapping `dispatch` to a dictionary of
+     * commands (functions) and pass it down with another context.
+     * 
+     * So for example, instead of writing
+     *
+     * dispatch({ type: ACTION.SWITCH_TO_TAB, data: TAB.ALL })
+     * (see src/components/SecondHeader/index.js)
+     *
+     * in the components, we would write
+     *
+     * commands.switchToTab(TAB.ALL)
+     *
+     * , which would call `dispatch` for us and is easier to read.
+     */
     <AppStateContext.Provider value={{ ...state, dispatch }}>
       <Card />
     </AppStateContext.Provider>
