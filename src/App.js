bf277fe54a40bbc40d2ffd803ac241e20a8860ff
Alexander Wang <zxcvb22217@gmail.com>
2021-10-10 15:59:35 +0800

    feat: Layout all components correctly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import styles from "./App.module.scss";
 import { Header } from "./components/Header";
-import { Footer } from './components/Footer'
+import { Footer } from "./components/Footer";
 import { SecondHeader } from "./components/SecondHeader";
+import { Content } from "./components/Content";
 
 function App() {
   return (
     <div className={styles.app}>
-      <Header />
-      <SecondHeader />
-      <Footer />
+      <div className={styles.headers}>
+        <Header />
+        <SecondHeader />
+      </div>
+      <div className={styles.content}>
+        <Content />
+      </div>
+      <div className={styles.footer}>
+        <Footer />
+      </div>
     </div>
   );
 }
